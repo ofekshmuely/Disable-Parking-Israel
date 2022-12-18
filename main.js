35be92aab6b53d9d8e0618b61d6588a9fadbbc7d
Ofek Shmuely <meshuba.global@gmail.com>
2022-12-18 15:43:57 +0200

    adding isNaN term

--- main.js
+++ main.js
@@ -1,67 +1,60 @@
-let e =document.getElementsByClassName("modalbox");
+let e = document.getElementsByClassName("modalbox");
 
-document.getElementById("close-modal").addEventListener("click", function() {
-document.getElementById("overlay").style.display = "none";
-document.getElementById("licenseplateBox temperature").focus();
-})
-
-
-
-
+document.getElementById("close-modal").addEventListener("click", function () {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("licenseplateBox temperature").focus();
+});
 
 async function getPlate() {
+  //let carLicensePlate = 91849601
 
+  let carLicensePlate = document.getElementById(
+    "licenseplateBox temperature"
+  ).value;
+  carLicensePlate = carLicensePlate.toString().replace(/\D/g, "");
 
-    //let carLicensePlate = 91849601
+  carLicensePlate = parseFloat(carLicensePlate);
 
-    let carLicensePlate = document.getElementById("licenseplateBox temperature").value;
-    carLicensePlate = carLicensePlate.toString().replace(/\D/g,'');
+  //fetching data from gov.il
+  let postsPromise = await fetch(
+    `https://data.gov.il/api/3/action/datastore_search?q=${carLicensePlate}&resource_id=c8b9f9c8-4612-4068-934f-d4acd2e3c06e`
+  );
+  let obj = await postsPromise.json();
+  let serachKey =
+    obj && obj.result && obj.result.records && obj.result.records.length
+      ? obj.result.records.map((data) => data)
+      : [];
 
-    carLicensePlate = parseFloat(carLicensePlate);
-    
+  let carLicensePlateResult = JSON.stringify(serachKey);
+  let isHandicaptedCarBoolean = carLicensePlateResult.includes(carLicensePlate);
 
+  console.log(
+    `variable isHandicaptedCarBoolean value when NaN: ${isHandicaptedCarBoolean}`
+  );
 
-    //fetching data from gov.il
-    let postsPromise = await fetch(`https://data.gov.il/api/3/action/datastore_search?q=${carLicensePlate}&resource_id=c8b9f9c8-4612-4068-934f-d4acd2e3c06e`);
-    let obj = await postsPromise.json(); 
-    let serachKey = obj && obj.result && obj.result.records && obj.result.records.length 
-    ? obj.result.records.map(data => data) : []; 
-    
-    let carLicensePlateResult = JSON.stringify(serachKey);
-    let isHandicaptedCarBoolean = carLicensePlateResult.includes(carLicensePlate);
+  //true modal
+  if (isHandicaptedCarBoolean === true) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("modal").style.backgroundColor = "#016148";
+    document.getElementById("carnum").textContent = carLicensePlate;
+    document.getElementById("statusReveal").textContent = "הרכב בעל תו נכה";
+  }
 
-
-    //true modal
-    if(isHandicaptedCarBoolean === true){
-        document.getElementById("overlay").style.display = "block";
-        document.getElementById("modal").style.backgroundColor  = "#016148";
-        document.getElementById("carnum").textContent = carLicensePlate;
-        document.getElementById("statusReveal").textContent = "הרכב בעל תו נכה";
-
-
-
-
+  //false modal
+  if (isHandicaptedCarBoolean === false) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("modal").style.backgroundColor = "#8B0000";
+    document.getElementById("carnum").textContent = carLicensePlate;
+    document.getElementById("statusReveal").textContent =
+      "הרכב אינו בעל תו נכה";
+    if (isNaN(carLicensePlate)) {
+      document.getElementById("statusReveal").textContent =
+        "לחץ על סגור והכנס מספר רכב תקין בשנית";
+      document.getElementById("carnum").textContent = "לא הוזן מספר רכב";
     }
+  }
 
-    //false modal
-    if(isHandicaptedCarBoolean === false){
-        document.getElementById("overlay").style.display = "block";
-        document.getElementById("modal").style.backgroundColor  = "#8B0000";
-        document.getElementById("carnum").textContent = carLicensePlate;
-        document.getElementById("statusReveal").textContent = "הרכב אינו בעל תו נכה";
-
-
-
-
-
-    }
-   
-    document.getElementById('licenseplateBox temperature').value = "";
-    document.getElementById("licenseplateBox temperature").focus();
-
-                                                                        }
-
-
-                                                                       
-
-     
+  //   Reseting form
+  document.getElementById("licenseplateBox temperature").value = "";
+  document.getElementById("licenseplateBox temperature").focus();
+}
